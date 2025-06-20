248023e614acb36292e09bf73d482af271bb993e
joaoxy <139055909+joaoxy@users.noreply.github.com>
2025-06-20 12:29:17 -0300

    Update pages.js

--- js/pages.js
+++ js/pages.js
@@ -289,7 +289,7 @@ function setupRegistrationPage() {
 // Setup event listeners for video page
 function setupVideoPage() {
   document.querySelector('.unlock-btn').addEventListener('click', () => {
-    window.location.href = 'https://go.disruptybr.shop/ftcauwkzmj';
+    window.location.href = 'https://go.tribopay.com.br/jox2w4ocgw';
   });
 
 }
